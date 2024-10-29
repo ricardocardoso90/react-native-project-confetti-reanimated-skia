3e03485771c023046dbe7d510fba96c0ca75e8d5
ricardocardoso90 <ricardomanolo90@gmail.com>
2024-10-29 15:37:33 -0300

    update commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import { Confetti, ConfettiMethods } from 'react-native-fast-confetti';
 
 export default function App() {
+  const confettiRef = useRef<ConfettiMethods>(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Confetti
+        autoplay={false}
+        ref={confettiRef}
+        colors={['#FF0000', '#0000FF']}
+        fadeOutOnEnd
+      />
+
+      <Button
+        title='Iniciar'
+        onPress={() => confettiRef.current?.restart()}
+      />
+
+      <Button
+        title='Pausar'
+        onPress={() => confettiRef.current?.pause()}
+      />
+
+      <Button
+        title='Continuar'
+        onPress={() => confettiRef.current?.resume()}
+      />
     </View>
   );
 }
